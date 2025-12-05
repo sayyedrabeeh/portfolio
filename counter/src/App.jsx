47977146ac48274dff26c1b05e23e1f36d913d78
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-12-05 09:20:55 +0530

    couter to portfolio

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import Counter from './pages/Counter'
+// import Counter from './pages/Counter'
+import Portfolio from './pages/recu';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
  
@@ -8,12 +9,15 @@ function App() {
    
 
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Counter />}>
-      </Route>
-      </Routes>
-      </BrowserRouter>
+  //   <BrowserRouter>
+  //   <Routes>
+  //       <Route path="/" element={<Counter />}>
+          
+  //     </Route>
+  //     </Routes>
+  //     </BrowserRouter>
+  // )
+    <Portfolio />
   )
 }
 
