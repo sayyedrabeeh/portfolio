75683446e1aff360c3403a07995fb76082dd7ab7
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2026-01-08 23:32:31 +0530

    Initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+// import Counter from './pages/Counter'
+import Portfolio from './pages/recu';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ 
+
+function App() {
+   
+
+  return (
+  //   <BrowserRouter>
+  //   <Routes>
+  //       <Route path="/" element={<Counter />}>
+          
+  //     </Route>
+  //     </Routes>
+  //     </BrowserRouter>
+  // )
+    <Portfolio />
+  )
+}
+
+export default App
